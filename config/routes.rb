9b6556387da5832ca7e62fa210d336f16a39fac1
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2017-04-05 00:03:10 +0200

    Add views. Add respositioning.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Ecm::Staff::Backend::Engine.routes.draw do
   backend_resources :organisations
   backend_resources :business_units
   backend_resources :positions
-  backend_resources :people
+  backend_resources :people do
+    post :reposition, on: :member
+  end
 
   root to: 'home#index'
 end
